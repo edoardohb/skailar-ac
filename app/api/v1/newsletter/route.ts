a0b813644fb9c4f50d2c2c747c46a6a28ab5717d
edoardohb <bergamo@edoardo-hb.it>
2024-11-30 17:17:44 +0100

    feat: limit newsletter

--- app/api/v1/newsletter/route.ts
+++ app/api/v1/newsletter/route.ts
@@ -1,5 +1,6 @@
 import { db } from "@/server";
 import { newsletter } from "@/server/schema";
+import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
@@ -22,13 +23,38 @@ export async function POST(req: NextRequest) {
       );
     }
 
+    const existingEmail = await db.query.newsletter.findFirst({
+      where: eq(newsletter.email, email),
+    });
+
+    if (existingEmail) {
+      return NextResponse.json(
+        { success: true, message: "Email already exists in newsletter." },
+        { status: 200 }
+      );
+    }
+
+    const alreadySubscribed = await db.query.newsletter.findFirst({
+      where: eq(newsletter.ip, ip),
+    });
+
+    if (alreadySubscribed) {
+      return NextResponse.json(
+        { success: true, message: "Email already exists in newsletter." },
+        { status: 200 }
+      );
+    }
+
     await db.insert(newsletter).values({
       id: uuidv4(),
       ip,
       email,
     });
 
-    return NextResponse.json({ success: true, message: "Email added to newsletter successfully." });
+    return NextResponse.json(
+      { success: true, message: "Email added to newsletter successfully." },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error adding email to newsletter:", error);
     return NextResponse.json(
